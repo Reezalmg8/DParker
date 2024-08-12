c37e77a8d7d5e44c3ee72963a6287b4e6f0bfccb
Reezal <swansea73@gmail.com>
2024-08-12 09:00:14 +0800

    fetch

--- js/registration.js
+++ js/registration.js
@@ -1,23 +1,28 @@
-import axios from 'axios';
-
+// registration.js
 document.querySelector('.register-form').addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent the default form submission
 
     const formData = new FormData(event.target);
     const data = Object.fromEntries(formData.entries());
 
-    axios.post('http://52.77.246.223:3002/register', data)
-        .then(response => {
-            const result = response.data;
-            if (result.message === 'User registered successfully!') {
-                alert(result.message);
-                location.href = 'login.html'; // Redirect to login page
-            } else {
-                alert('Registration failed: ' + result.message);
-            }
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            alert('Registration failed. Please try again.');
-        });
+fetch('http://52.77.246.223:3002/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    })
+    .then(response => response.json())
+    .then(result => {
+        if (result.message === 'User registered successfully!') {
+            alert(result.message);
+            location.href = 'login.html'; // Redirect to login page
+        } else {
+            alert('Registration failed: ' + result.message);
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        alert('Registration failed. Please try again.');
+    });
 });
